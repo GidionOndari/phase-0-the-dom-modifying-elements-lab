b3319b377c8961c4994534f10fa92be5bb431ef6
ondarigidion <gidion.ondari@student.moringaschool.com>
2025-07-01 11:53:35 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code here!
+// Remove the <main> node
+const main = document.getElementById('main');
+main.remove(); // or document.querySelector('main#main').remove()
+
+// Create a new <h1>
+const newHeader = document.createElement('h1');
+
+// Give it an id
+newHeader.id = 'victory';
+
+// Set the text inside it (use textContent or innerHTML, NOT innerText)
+newHeader.textContent = 'Gidion is the champion'; //
+
+// Add it to the DOM (append to body)
+document.body.append(newHeader);
